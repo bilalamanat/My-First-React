d60d9d00b43b62f7512a96e5a61d83b56241db41
Bilal Amanat Muhammadi <136390430+bilalamanat@users.noreply.github.com>
2023-10-27 15:05:30 +0500

    WhatSapp Button Added:

--- src/Website/Totay/Call.jsx
+++ src/Website/Totay/Call.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-function Call () {
+function Call() {
   return (
-    <div> 
-         <section className="cta">
-    <h1>Enroll For Oue Various Online Courses <br />Anywhere From The World</h1>
-    <a href className="hero-btn">Contact Us</a>
-  </section>
-  </div>
-  )
+    <div>
+      <section className="cta">
+        <h1>Enroll For Our Various Online Courses <br />Anywhere From The World</h1>
+        <a href="#" className="hero-btn">Contact Us</a>
+      </section>
+    </div>
+  );
 }
 
-export default Call 
+export default Call;
